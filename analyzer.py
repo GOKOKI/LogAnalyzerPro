0d41f7b5f1497cab35a4bd0905337879f5ae3941
eje019 <emricndayake@gmail.com>
2026-03-18 13:02:35 +0000

    Test rapide

--- analyzer.py
+++ analyzer.py
@@ -1,10 +1,3 @@
-# Exemple imaginaire
-import argparse
 
-# On crée le "menu"
-analyseur = argparse.ArgumentParser(description="Outil d'analyse de logs")
-analyseur.add_argument("--couleur", help="La couleur préférée")
 
-# L'utilisateur tape : python script.py --couleur rouge
-args = analyseur.parse_args()
-print(args.couleur)  # Affiche "rouge"
+print("Bonjour je suis l'analyseur de LOGS")
