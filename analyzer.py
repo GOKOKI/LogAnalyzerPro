b89cb87d657b16dcbe5e88991edbc97ba4d02e9b
eje019 <emricndayake@gmail.com>
2026-03-23 13:29:49 +0100

    Feat : Amélioration de l'affichage des niveaux de logs et comptage des messages d'erreur

--- analyzer.py
+++ analyzer.py
@@ -13,9 +13,10 @@ args = analyseur.parse_args()
 fichiers_log = glob.glob(args.source + "/*.log")
 
 # print(f"Le dossier a analyser est :",args.source, "au niveau :" ,args.niveau)
+niveau = args.niveau 
 
 print("Dossier : ", args.source)
-print("Niveau : ", args.niveau)
+print("Niveau : ", niveau)
 print("Fichiers trouvés :", fichiers_log)
 
 
@@ -23,14 +24,15 @@ total_lignes = 0
 compte_INFO = 0
 compte_WARN = 0
 compte_ERROR = 0
+erreurs = {} #dictionnaire pour compter chaque message d'erreur 
 
 for fichier in fichiers_log:
     # ouverture du fichier en mode lecturee seule
     with open(fichier, "r", encoding="utf-8") as f:
-        lignes = f.readlines() #on lit toutes les lignes du fichier
+        # lignes = f.readlines() #on lit toutes les lignes du fichier
         # total_lignes = total_lignes + len(lignes) #on ajoute le nombre de lignes au total
-
-
+        
+        
         # gestion des filtres pour les niveaux
         for ligne in f:
             total_lignes = total_lignes + 1
@@ -44,8 +46,24 @@ for fichier in fichiers_log:
                 compte_ERROR = compte_ERROR + 1
         # print(fichier, "contient", len(lignes), "lignes") #on dit ce fichier contient combien de lignes d'abord
 
+        morceaux = ligne.split() #la ligne est decoupee
+
+        message = " ".join(morceaux[3:])
+        
+        if message in erreurs:
+            erreurs[message] = erreurs[message] + 1
+        else:
+            erreurs[message] = 1
+
 print("----Resultats----")
 print("Total de toutes les lignes :", total_lignes) #on affiche le nombre total en general
-print("INFO :", compte_INFO)
-print("ERROR :", compte_ERROR)
-print("WARN :", compte_WARN)
+# print("INFO :", compte_INFO)
+# print("ERROR :", compte_ERROR)
+# print("WARN :", compte_WARN)
+
+if niveau == "ALL" or niveau == "INFO":
+    print("INFO :", compte_INFO)
+if niveau == "ALL" or niveau == "WARN":
+    print("WARN :", compte_WARN)
+if niveau == "ALL" or niveau == "ERROR":
+    print("ERROR :", compte_ERROR)
